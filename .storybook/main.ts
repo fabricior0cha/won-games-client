84894d5a79fa49b555cbf6235af7ec155d1d7743
fabricior0cha <fabriciorochapalmeira@gmail.com>
2023-11-12 10:18:39 -0300

    feat: config theme

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,10 +8,7 @@ const config = {
   },
   docs: {
     autodocs: true
-  },
-  webpackFinal: (config) => {
-    config.resolve.modules.push(`${process.cwd()}/src`)
-    return config
   }
 }
+
 export default config
